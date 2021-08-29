443f40d6d49427cecfc63c29c33e8afc26aa31ff
Felipe Merchan <felipe-sc@hotmail.es>
2021-08-29 18:21:21 -0500

    style: initial structure

---
+++ src/components/TodoItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './styles/TodoItem.css';
+
+const TodoItem = ({ text, completed }) => {
+  return (
+    <li className="TodoItem">
+      <span className={`Icon Icon-check ${completed && 'Icon-check--active'}`}>
+        √
+      </span>
+      <p className={`TodoItem-p ${completed && 'TodoItem-p--complete'}`}>
+        {text}
+      </p>
+      <span className="Icon Icon-delete">
+        X
+      </span>
+    </li>
+  );
+};
+
+export { TodoItem };
---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/TodoList.css';
+
+const TodoList = ({ children }) => {
+  return (
+    <section>
+      <ul>
+        {children}
+      </ul>
+    </section>
+  );
+};
+
+export { TodoList };
