43289bdc02589f5da76d2cc4c787c13f8e4c1a88
Felipe Merchan <felipe-sc@hotmail.es>
2021-10-03 19:59:48 -0500

    feat: create form

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -4,7 +4,7 @@ import './CreateTodoButton.css';
 
 const CreateTodoButton = ({ setOpenModal }) => {
   const handleClick = () => {
-    setOpenModal(true);
+    setOpenModal(prevState => !prevState);
   };
 
   return (
